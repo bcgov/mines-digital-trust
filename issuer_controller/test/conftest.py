6f4e4c8db95791126c8c1faed1cf8d4f8a8f7ecf
Jason Sy <jasyrotuck@gmail.com>
2020-12-10 11:52:59 -0800

    manually execute all side effect of StartupThread to test in isolation.

--- issuer_controller/test/conftest.py
+++ issuer_controller/test/conftest.py
@@ -3,12 +3,17 @@ import pytest, os, pprint
 from app.app import create_app
 from app import config, issuer
 
-
+from unittest.mock import patch
+from requests.models import Response
 from app.config import TestConfig
 
 @pytest.fixture(scope="session")
 def app(request):
-    # Load application settings (environment)
+    # # Load application settings (environment)
+    # mock_tob_connection_response = Response(_content=json.dumps(mock_tob_connection_json))
+    # with patch('app.issuer.requests.get', side_effect=[response])as mock:
+    config_root = os.environ.get('CONFIG_ROOT', './config')
+    ENV = config.load_settings(config_root=config_root)
     app = create_app(TestConfig)
     return app
 
