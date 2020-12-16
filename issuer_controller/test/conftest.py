ae4e8589c35abf24e66bc50f70028b3a0454b9ed
Jason Sy <jasyrotuck@gmail.com>
2020-12-16 10:00:25 -0800

    use test config if env dictionary contains mode=test
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- issuer_controller/test/conftest.py
+++ issuer_controller/test/conftest.py
@@ -9,11 +9,6 @@ from app.config import TestConfig
 
 @pytest.fixture(scope="session")
 def app(request):
-    # # Load application settings (environment)
-    # mock_tob_connection_response = Response(_content=json.dumps(mock_tob_connection_json))
-    # with patch('app.issuer.requests.get', side_effect=[response])as mock:
-    config_root = os.environ.get('CONFIG_ROOT', './config')
-    ENV = config.load_settings(config_root=config_root)
     app = create_app(TestConfig)
     return app
 
@@ -22,6 +17,5 @@ def test_client(app):
     client = app.test_client()
     ctx = app.app_context()
     ctx.push()
-    #print(app.url_map)
     yield client
     ctx.pop()
