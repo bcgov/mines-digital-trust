0ed455a59f54377039c48525da01d4811abcc24a
Jason Sy <jasyrotuck@gmail.com>
2021-01-15 15:44:19 -0800

    curated changes made during deployment
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- openshift/templates/mines-permitting-issuer/mines-permitting-issuer-deploy.overrides.sh
+++ openshift/templates/mines-permitting-issuer/mines-permitting-issuer-deploy.overrides.sh
@@ -16,18 +16,18 @@ fi
 if createOperation; then
   readParameter "CR_AGENT_ADMIN_URL - Please provide the URL for the credential registry admin api.\nThe default is an empty string:" "CR_AGENT_ADMIN_URL" "" "false"
   readParameter "CR_ADMIN_API_KEY - Please provide the key for the credential registry admin api.\nThe default is an empty string:" "CR_ADMIN_API_KEY" "" "false"
-  readParameter "CR_CONNECTION_NAME - Please provide the key for the credential registry connection/agent name.\nThe default is: icob-agent " "CR_CONNECTION_NAME" "icob-agent" "false"
-  readParameter "CR_API_URL - Please provide the key for the credential registry API url.\nThe default is: https://agent-dev.orgbook.gov.bc.ca " "CR_API_URL" "https://agent-dev.orgbook.gov.bc.ca" "false"
-  readParameter "CR_APP_URL - Please provide the key for the credential registry application url.\nThe default is https://dev.orgbook.gov.bc.ca " "CR_APP_URL" "https://dev.orgbook.gov.bc.ca" "false"
+  #readParameter "CR_CONNECTION_NAME - Please provide the key for the credential registry connection/agent name.\nThe default is: icob-agent " "CR_CONNECTION_NAME" "icob-agent" "false"
+  #readParameter "CR_API_URL - Please provide the key for the credential registry API url.\nThe default is: https://agent-dev.orgbook.gov.bc.ca " "CR_API_URL" "https://agent-dev.orgbook.gov.bc.ca" "false"
+  #readParameter "CR_APP_URL - Please provide the key for the credential registry application url.\nThe default is https://dev.orgbook.gov.bc.ca " "CR_APP_URL" "https://dev.orgbook.gov.bc.ca" "false"
 else
   # Secrets are removed from the configurations during update operations ...
   printStatusMsg "Update operation detected ...\nSkipping the prompts for the CR_* secrets ...\n"
   # Prompted
   writeParameter "CR_AGENT_ADMIN_URL" "generation_skipped" "false"
   writeParameter "CR_ADMIN_API_KEY" "generation_skipped" "false"
-  writeParameter "CR_CONNECTION_NAME" "generation_skipped" "false"
-  writeParameter "CR_API_URL" "generation_skipped" "false"
-  writeParameter "CR_APP_URL" "generation_skipped" "false"
+  #writeParameter "CR_CONNECTION_NAME" "generation_skipped" "false"
+  #writeParameter "CR_API_URL" "generation_skipped" "false"
+  #writeParameter "CR_APP_URL" "generation_skipped" "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
