703d4fbe7f66cfb1631fb72ca0be738ccdf7a488
Jason Sherman <jsherman@parcsystems.ca>
2021-02-11 16:13:58 -0800

    Python scripts to issue GHG Credentials Update the GHG Credential facility location attributes.
    Signed-off-by: Jason Sherman <jsherman@parcsystems.ca>

---
+++ ghg_issuer_tools/parse.py
@@ -0,0 +1,200 @@
+import argparse
+import json
+import pathlib
+from datetime import date
+
+import pandas as pd
+import yaml
+
+DEFAULT_SCHEMA_NAME = 'ggirc-act.ghg-emissions-report'
+DEFAULT_SCHEMA_VERSION = '0.2.0'
+DEFAULT_ATTRIBUTES = [
+    'registration_id',
+    'facility_name',
+    'facility_latitude',
+    'facility_longitude',
+    'primary_activity_code',
+    'primary_activity_description',
+    'co2_fossil_emissions',
+    'co2_biomass_emissions',
+    'ch4_emissions',
+    'n2o_emissions',
+    'hfcs_emissions',
+    'pfcs_emissions',
+    'sf6_emissions',
+    'verification_body',
+    'verification_result',
+    'reporting_year',
+    'issued_date',
+    'effective_date'
+]
+
+
+parser = argparse.ArgumentParser(
+    description='Parse GHG Emissions CSV to Verified Credentials')
+
+parser.add_argument('--csv', '-c',
+                    dest='csv_file',
+                    help='Path to CSV file (defaults to ghg-emissions.csv)',
+                    default='ghg-emissions.csv')
+parser.add_argument('--year', '-y',
+                    dest='year',
+                    help='Reporting Year (optional)')
+parser.add_argument('--schemas', '-s',
+                    dest='schema_file',
+                    help='Path to schemas file (optional)')
+parser.add_argument('--name', '-n',
+                    dest='schema_name',
+                    help='Schema name (optional)')
+parser.add_argument('--version', '-v',
+                    dest='schema_version',
+                    help='Schema version (optional)')
+parser.add_argument('--mapping', '-m',
+                    dest='csv_schema_mapping',
+                    help='CSV to Schema Mappings (optional)')
+parser.add_argument('--registry', '-r',
+                    dest='company_registry',
+                    help='Company to BC Registration ID Mappings (optional)')
+
+
+def parse_csv(csv_file,
+              year,
+              schema_file,
+              schema_name,
+              schema_version,
+              csv_schema_mapping,
+              company_registry):
+
+    _today = date.today().isoformat()
+
+    _schema_name = DEFAULT_SCHEMA_NAME
+    _schema_version = DEFAULT_SCHEMA_VERSION
+    _schema_attributes = DEFAULT_ATTRIBUTES
+
+    _mappings = None
+    _company_registry = None
+
+    if not pathlib.Path(csv_file).exists():
+        print(f'CSV File \'{csv_file}\' not found. Exiting.')
+        exit(1)
+
+    if schema_file and pathlib.Path(schema_file).exists():
+        # we want to override the schema, we need
+        schemas = yaml.safe_load(open(schema_file, 'r', encoding='utf-8'))
+        schema = next(filter(lambda x: x['name'] == schema_name and x['version'] == schema_version, schemas), None)
+
+        if schema is None:
+            print(f"Schema '{schema_name}/{schema_version}' not found in Schemas File. Exiting.")
+            exit(1)
+        else:
+            _schema_name = schema['name']
+            _schema_version = schema['version']
+            _schema_attributes = schema['attributes']
+
+    if csv_schema_mapping and pathlib.Path(csv_schema_mapping).exists():
+        _mappings = pd.read_csv(csv_schema_mapping, index_col=False)
+
+    if company_registry and pathlib.Path(company_registry).exists():
+        co_reg = pd.read_csv(company_registry, index_col=0, header=0, skiprows=0,
+                             keep_default_na=False)
+        _company_registry = co_reg.to_dict()
+
+    _emissions = pd.read_csv(csv_file, index_col=False, header=0, skiprows=0, keep_default_na=True)
+
+    if 'activity' in _emissions.columns:
+        # split the activity column into primary_activity_code and primary_activity_description
+        # let's hope those attributes do not get their names changed from version to version.
+        _emissions[['primary_activity_code',
+            'primary_activity_description']] = _emissions.activity.str.split('--', expand=True, )
+
+    def nice_value(value):
+        # for our purposes, we cannot have None or null
+        # and everything is a string...
+        if pd.isna(value) or value is None:
+            return ''
+
+        return str(value)
+
+    def get_value(name, data):
+
+        if name == 'registration_id':
+            try:
+                # is registration_id in emissions data?
+                return nice_value(data[name])
+            except KeyError:
+                # then look up in company registry (emissions csv should have company_name column)
+                co_name = data['company_name']
+                return nice_value(_company_registry['registration_id'][co_name])
+
+        if name == 'reporting_year':
+            try:
+                # is reporting_year in emissions data?
+                return nice_value(data[name])
+            except KeyError:
+                # then we should have passed in a year...
+                return nice_value(year)
+
+        if name == 'issued_date':
+            try:
+                # is issued_date in emissions data?
+                return nice_value(data[name])
+            except KeyError:
+                # then let's use today
+                return nice_value(_today)
+
+        if name == 'effective_date':
+            try:
+                # is effective_date in emissions data?
+                return nice_value(data[name])
+            except KeyError:
+                # then let's use today
+                return nice_value(_today)
+
+        try:
+            # schema attribute name is a emissions csv column
+            return nice_value(data[name])
+        except KeyError:
+            # ok, there should be a mapping for it then
+            # find the column name mapping used for this schema attribute...
+            csv_name = _mappings.iloc[0:1][name].values[0]
+            # grab the value
+            value = data[csv_name]
+            return nice_value(value)
+
+    def get_attributes(names, data):
+        result = {}
+        for name in names:
+            result[name] = get_value(name, data)
+
+        return result
+
+    vcs = []
+    for index, row in _emissions.iterrows():
+        try:
+            vc = {'schema': _schema_name,
+                  'version': _schema_version,
+                  'attributes': get_attributes(_schema_attributes, row)
+                  }
+            vcs.append(vc)
+        except KeyError:
+            print(f'Error creating VC for line {index+2}')
+
+    return json.dumps(vcs)
+
+
+def main():
+    args = parser.parse_args()
+
+    result = parse_csv(csv_file=args.csv_file,
+                       year=args.year,
+                       schema_file=args.schema_file,
+                       schema_name=args.schema_name,
+                       schema_version=args.schema_version,
+                       csv_schema_mapping=args.csv_schema_mapping,
+                       company_registry=args.company_registry)
+
+    print(json.dumps(json.loads(result), indent=4))
+
+
+if __name__ == "__main__":
+    main()
