5ae5027fa36f827282c66d27f47f44fbfa8afa18
Jason Sy <jasyrotuck@gmail.com>
2021-03-08 14:31:39 -0800

    one dockerfile - 2 agents
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

---
+++ docker/bpa-network/register-dids.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Copyright (c) 2020 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository at
+# https://github.com/hyperledger-labs/organizational-agent
+# 
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Check the system the script is running on
+ARCHITECTURE="$(uname -s)"
+if [[ ${ARCHITECTURE} == "Linux"* ]]; then
+    ARCHITECTURE="Linux"
+elif [[ ${ARCHITECTURE} == "Darwin"* ]]; then
+    ARCHITECTURE="Mac"
+fi
+
+if [ "$ARCHITECTURE" != "Linux" ] && [ "$ARCHITECTURE" != "Mac" ]; then
+    echo "No Linux or Mac OSX detected. You might need to do some steps manually."
+fi
+
+if [ ! -x "$(which curl)" ] ; then
+    echo "Couldn't find curl. Please make sure that curl is installed."
+    exit 1
+fi
+
+
+# Set URL
+URL=${LEDGER_URL:-https://indy-test.bosch-digital.de}
+
+# Set random alias
+ALIAS=BPA-$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+# Generate random seed
+SEED1=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+SEED2=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+PAYLOAD='{"alias":"'"$ALIAS"'","seed":"'"$SEED"'","role":"ENDORSER"}'
+
+# Register DID
+if curl --fail -s -d $PAYLOAD  -H "Content-Type: application/json" -X POST ${URL}/register; then
+    # Registration (probably) successfull
+    echo ""
+    echo ""Registration on $URL successful""
+    echo ""Setting ACAPY_SEED in .env file""
+    if [ ! -f .env ]; then
+        echo "".env does not exist""
+        echo ""Creating .env from .env-example""
+        cp .env-example .env
+    fi
+    # sed on Mac and Linux work differently
+    if [ "$ARCHITECTURE" = "Mac" ]; then
+        sed -i '' '/ACAPY_SEED1=/c\
+        ACAPY_SEED1='"${SEED1}"'
+        ' .env
+        sed -i '' '/ACAPY_SEED2=/c\
+        ACAPY_SEED2='"${SEED2}"'
+        ' .env
+    else
+         sed -i '/ACAPY_SEED1=/c\
+        ACAPY_SEED1='"${SEED1}"'
+        ' .env
+        sed -i '/ACAPY_SEED2=/c\
+        ACAPY_SEED2='"${SEED2}"'
+        ' .env
+    fi 
+    
+else
+    # Something went wrong
+    echo ""
+    echo Something went wrong
+fi;
