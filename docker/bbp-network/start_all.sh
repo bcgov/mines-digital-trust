2e28ea23db16012209c1c9dcd3aa6c2d14f0e3a5
Jason Sy <jasyrotuck@gmail.com>
2021-04-06 12:01:24 -0700

    no default schemas.
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- docker/bbp-network/start_all.sh
+++ docker/bbp-network/start_all.sh
@@ -6,14 +6,14 @@ git clone https://github.com/bcgov/von-network.git von-network
 ./von-network/manage start
 
 #GET LOCAL LEDGER GENESIS FILES AND START RESOLVER
-curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o ./localhost_9000.txn
+curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o resolver/localhost_9000.txn
 
 while [ $? -ne 0 ];
 do
     sleep 3
     curl --retry-connrefused --retry 5 --retry-delay 2 http://localhost:9000/genesis -o resolver/localhost_9000.txn
 done
-docker-compose up -d driver-did-sov 
+docker-compose -f ./resolver/docker-compose.yml up -d driver-did-sov 
 
 #START BB-PacMans
 docker-compose up bpa1 bpa-agent1 bpa2 bpa-agent2
